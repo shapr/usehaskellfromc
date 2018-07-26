8b51e3b53a98e41ed9efd78b57656fe2f34c8610
chessai <chessai1996@gmail.com>
2018-07-25 21:26:53 -0400

    optimise fib

--- Foo.hs
+++ Foo.hs
@@ -1,15 +1,22 @@
 {-# LANGUAGE BangPatterns             #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
+
 module Foo where
 
 import Foreign
 import Foreign.C.Types
 
-fib n = go n (0,1)
+data IntPair = IntPair !Int !Int
+
+fib :: Int -> Int
+fib n = go n (IntPair 0 1)
   where
-    go !n (!a, !b) | n == 0    = a
-                   | otherwise = go (n-1) (b, a+b)
+    go !n (IntPair !a !b)
+      | n == 0    = a
+      | otherwise = go (n-1) (IntPair b (a + b))
+{-# INLINE fib #-}
 
 fib_hs :: CInt -> CInt
 fib_hs = fromIntegral . fib . fromIntegral
+
 foreign export ccall fib_hs :: CInt -> CInt
